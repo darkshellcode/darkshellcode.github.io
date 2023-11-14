89fb9a7316c7e430057ce341c0b28232470dfe74
darkshellcode <sandromelo@tutanota.com>
2023-11-14 02:37:47 -0300

    teste deploy novo

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699939212';
+const cacheName = 'chirpy-1699939401';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
